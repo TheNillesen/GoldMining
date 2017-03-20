2e0b3e5ab0b880fa4f67effebc3da34fbdc77f48
Nvhen <Nvhen@LAPTOP-VUTMMO7N>
2017-03-20 11:59:03 +0100

    awa

--- GoldMiningString/GoldMiningString/GameObject.cs
+++ GoldMiningString/GoldMiningString/GameObject.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace GoldMiningString
 {
-    class GameObject
+   abstract class GameObject
     {
+        /// <summary>
+        /// The GameObject's position
+        /// </summary>
+        protected Vector2 position;
+
+        /// <summary>
+        /// The GameObject's sprite
+        /// </summary>
+        protected Texture2D sprite;
+        protected string spriteName;
+        protected float scale;
+
+
+        /// <summary>
+        /// The GameObject's constructor
+        /// </summary>
+        public GameObject(Vector2 position, string spriteName, float scale)
+        {
+            this.position = position;
+            this.spriteName = spriteName;
+            this.scale = scale;
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            sprite = content.Load<Texture2D>(spriteName);
+        }
+        public void update()
+        {
+            //blank untill we fire something in there
+        }
+
     }
 }
